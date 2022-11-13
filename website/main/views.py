958d097f430d6b6ae1a71f2983b9dcd4447af5b5
Artem Balakirev <artembalakirev820@gmail.com>
2022-11-13 21:54:52 +0300

    sw

--- website/main/views.py
+++ website/main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_page(request):
-    return render(request, 'main/main_page.html')
+    return render(request, 'main/index.html')
